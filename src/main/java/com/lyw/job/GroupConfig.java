f59ee59c15548290bb2e715bd077fed4090e0b4b
yiweiliang1 <yiweiliang1@creditease.cn>
2018-11-02 18:37:55 +0800

    色图bot

--- src/main/java/com/lyw/job/GroupConfig.java
+++ src/main/java/com/lyw/job/GroupConfig.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class GroupConfig {
 
-    private static List<Long> jobs = Arrays.asList(596917621L);
+    private static List<Long> jobs = Arrays.asList(596917621L, 565759137L);
 
     public static List<Long> getJobsList() {
         return jobs;
